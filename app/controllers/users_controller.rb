11d3e85473c289b6c289c86f8f7e1af2919f1683
maik_zeyen <maik_zeyen@web.de>
2017-10-10 17:53:28 +0000

    Edit user via UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,29 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit 
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Daten für #{@user.username} erfolgreich geändert"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show 
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+  
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
